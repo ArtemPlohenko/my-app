293e24f7cd7da798a6aab33f2ab50bb672ed8bca
Artem Plohenko <artemplohenko@gmail.com>
2022-03-26 09:45:54 +0200

    added holder

--- src/App.js
+++ src/App.js
@@ -13,19 +13,21 @@ function App() {
           setSearchTerm(event.target.value);
         }}
       />
-      {JSONDATE.filter((val) => {
-        if (searchTerm === "") {
-          return val
-        } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return val
-        }
-      }).map((val, key) => {
-        return (
-          <div className="user" key={key}>
-            <p>{val.first_name} </p>
-          </div>
-        );
-      })}
+      <div className='holder-block'>
+        {JSONDATE.filter((val) => {
+          if (searchTerm === "") {
+            return val
+          } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return val
+          }
+        }).map((val, key) => {
+          return (
+            <div className="user" key={key}>
+              <p>{val.first_name} </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
